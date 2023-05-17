1410eb48ac1418ec8ab2b4ff876cd0cfa97060fa
Turgut Aydemir <turgut.aydemir@compax.at>
2023-05-17 20:30:57 +0200

    proto files changed

--- application.py
+++ application.py
@@ -4,6 +4,7 @@ import currency_converter_pb2
 import currency_converter_pb2_grpc
 from forex_python.converter import CurrencyRates
 
+
 class CurrencyConverterServicer(currency_converter_pb2_grpc.CurrencyConverterServicer):
     def ConvertCurrency(self, request, context):
         amount = request.amount
@@ -15,13 +16,15 @@ class CurrencyConverterServicer(currency_converter_pb2_grpc.CurrencyConverterSer
 
         return currency_converter_pb2.CurrencyConversionResponse(result=result)
 
+
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     currency_converter_pb2_grpc.add_CurrencyConverterServicer_to_server(CurrencyConverterServicer(), server)
-    server.add_insecure_port('[::]:50051')
+    # server.add_insecure_port('[::]:50051')
     server.start()
     print("Server started. Listening on port 50051.")
     server.wait_for_termination()
+
 
 if __name__ == '__main__':
     serve()
