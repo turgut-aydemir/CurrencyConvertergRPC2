fd83632dfc388fe579d7aa6aa3232752b5efb00e
Turgut Aydemir <turgut.aydemir@compax.at>
2023-05-17 20:48:54 +0200

    proto files changed

--- application.py
+++ application.py
@@ -20,7 +20,7 @@ class CurrencyConverterServicer(currency_converter_pb2_grpc.CurrencyConverterSer
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     currency_converter_pb2_grpc.add_CurrencyConverterServicer_to_server(CurrencyConverterServicer(), server)
-    # server.add_insecure_port('[::]:50051')
+    server.add_insecure_port('[::]:50051')
     server.start()
     print("Server started. Listening on port 50051.")
     server.wait_for_termination()
