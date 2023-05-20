56ad47335402ceb6df9129d2a4139d4222fe5aeb
turgut aydemir <turgut.aydemir@stud.fh-campuswien.ac.at>
2023-05-21 00:12:35 +0200

    maybe forex cannot be invoked without apikey, just trying

--- application.py
+++ application.py
@@ -7,19 +7,22 @@ from forex_python.converter import CurrencyRates
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def func():
     return "Currency Converter Server, serving with Http/1.1"
+
 
 class CurrencyConverterServicer(currency_converter_pb2_grpc.CurrencyConverterServicer):
     def ConvertCurrency(self, request, context):
         amount = request.amount
         from_currency = request.from_currency
         to_currency = request.to_currency
-        c = CurrencyRates()
-        rate = c.get_rate(from_currency, to_currency)
-        result = amount * rate
+        # c = CurrencyRates()
+        # rate = c.get_rate(from_currency, to_currency)
+        result = amount * 2  # rate
         return currency_converter_pb2.CurrencyConversionResponse(result=result)
+
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
@@ -27,6 +30,7 @@ def serve():
     p = server.add_insecure_port('0.0.0.0:8282')
     server.start()
     return server
+
 
 if __name__ == '__main__':
     grpc_server = serve()
