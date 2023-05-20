363ce1c228f8005b1019ea8aebeb647e131aa11e
turgut aydemir <turgut.aydemir@stud.fh-campuswien.ac.at>
2023-05-20 19:11:00 +0200

    changed format

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ import grpc
 import currency_converter_pb2
 import currency_converter_pb2_grpc
 from forex_python.converter import CurrencyRates
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -18,10 +18,8 @@ class CurrencyConverterServicer(currency_converter_pb2_grpc.CurrencyConverterSer
         amount = request.amount
         from_currency = request.from_currency
         to_currency = request.to_currency
-
         c = CurrencyRates()
-        conversion_rate = c.get_rate(from_currency, to_currency)
-        result = amount * conversion_rate
+        result = c.convert(from_currency, to_currency, amount)
 
         return currency_converter_pb2.CurrencyConversionResponse(result=result)
 
@@ -29,16 +27,12 @@ class CurrencyConverterServicer(currency_converter_pb2_grpc.CurrencyConverterSer
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     currency_converter_pb2_grpc.add_CurrencyConverterServicer_to_server(CurrencyConverterServicer(), server)
-    app.logger.error("Adding insecure port 8282")
-    p = server.add_insecure_port('0.0.0.0:8282')
-    app.logger.error("Opened up on port ")
-    app.logger.error(p)
+    server.add_insecure_port('[::]:8282')
     server.start()
-    return server
+    print("Server started. Listening on port 8282.")
+    server.wait_for_termination()
 
 
 if __name__ == '__main__':
-    app.logger.error("Hello! Starting up")
-    grpc_server = serve()
-    app.logger.error("Serving gRPC!")
-    app.run(host="0.0.0.0", port=8000)
+    serve()
+    app.run(host='0.0.0.0', port=8000)
