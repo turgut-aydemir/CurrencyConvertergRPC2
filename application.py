38c8807167d00a0dfb16507d9f35e60e4e6550f0
turgut aydemir <turgut.aydemir@stud.fh-campuswien.ac.at>
2023-05-21 22:37:48 +0200

    server changed and simplified

--- application.py
+++ application.py
@@ -2,31 +2,32 @@ from concurrent import futures
 import grpc
 import currency_converter_pb2
 import currency_converter_pb2_grpc
-from flask import Flask, request
+from flask import Flask
+from forex_python.converter import CurrencyRates
 
 app = Flask(__name__)
 
 @app.route('/')
 def func():
-    return "Currency Converter Server, serving with HTTP/1.1"
+    return "Currency Converter Server, serving with Http/1.1"
 
 class CurrencyConverterServicer(currency_converter_pb2_grpc.CurrencyConverterServicer):
     def ConvertCurrency(self, request, context):
         amount = request.amount
         from_currency = request.from_currency
         to_currency = request.to_currency
-
-        # TODO: Add currency conversion logic here
-
-        return currency_converter_pb2.CurrencyConversionResponse(result=2)  # Replace 2 with the actual rate
+        c = CurrencyRates()
+        rate = c.get_rate(from_currency, to_currency)
+        result = amount * rate
+        return currency_converter_pb2.CurrencyConversionResponse(result=result)
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     currency_converter_pb2_grpc.add_CurrencyConverterServicer_to_server(CurrencyConverterServicer(), server)
-    p = server.add_insecure_port('0.0.0.0:8282')
+    server.add_insecure_port('[::]:8282')
     server.start()
     return server
 
 if __name__ == '__main__':
-    serve()
-    app.run(host='0.0.0.0', port=8000)
+    grpc_server = serve()
+    app.run(host="0.0.0.0", port=8000)
